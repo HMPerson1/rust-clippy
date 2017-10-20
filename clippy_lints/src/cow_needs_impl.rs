347e527a3b63e6621a969f028727ee2494c1684a
HMPerson1 <hmperson1@gmail.com>
2017-10-19 20:56:56 -0400

    blarg

--- clippy_lints/src/cow_needs_impl.rs
+++ clippy_lints/src/cow_needs_impl.rs
@@ -1,4 +1,6 @@
 use rustc::lint::*;
+use rustc::hir;
+use rustc::hir::def_id::DefId;
 
 /// **What it does:** TODO
 ///
@@ -25,5 +27,22 @@ impl LintPass for CowNeedsImpl {
 
 impl<'a, 'tcx> LateLintPass<'a, 'tcx> for CowNeedsImpl {
     fn check_item(&mut self, cx: &LateContext<'a, 'tcx>, item: &'tcx hir::Item) {
+        let def_id = cx.tcx.hir.local_def_id(item.id);
+        match item.node {
+            hir::ItemTrait(..) =>
+                check_new_trait(cx, def_id),
+            hir::ItemEnum(..) | hir::ItemStruct(..) | hir::ItemUnion(..) =>
+                check_new_type(cx, def_id),
+            _ => {}
+        }
     }
 }
+
+fn check_new_trait(cx: &LateContext, trait_def_id: DefId) {
+    let trait_impls = cx.tcx.hir.trait_impls(trait_def_id);
+    for &trait_impl_id in trait_impls {
+        eprintln!("{:?}", cx.tcx.impl_trait_ref(cx.tcx.hir.local_def_id(trait_impl_id)));
+    }
+}
+
+fn check_new_type(cx: &LateContext, def_id: DefId) {}
--- tests/ui/cow_needs_impl.rs
+++ tests/ui/cow_needs_impl.rs
@@ -1,1 +1,28 @@
 #![deny(cow_needs_impl)]
+#![allow(dead_code)]
+
+use std::borrow::Cow;
+
+struct Type1;
+
+trait Trait1 {}
+
+impl Trait1 for Type1 {}
+
+trait Trait2 {
+    fn do_trait_2(&self);
+}
+
+impl<'a, T> Trait2 for Cow<'a, T>
+    where T: ToOwned + Trait2,
+          <T as ToOwned>::Owned: Trait2
+{
+    fn do_trait_2(&self) {
+        match *self {
+            Cow::Owned(ref o) => o.do_trait_2(),
+            Cow::Borrowed(b) => b.do_trait_2(),
+        }
+    }
+}
+
+fn main() {}
